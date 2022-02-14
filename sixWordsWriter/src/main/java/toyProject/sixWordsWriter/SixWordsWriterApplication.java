00e1003883fcf5ce8ca37a7e0140244ca2ded900
SeungHa Lim <tmdgk490255@gmail.com>
2022-02-14 19:42:08 +0900

    Initial Commit

---
+++ sixWordsWriter/src/main/java/toyProject/sixWordsWriter/SixWordsWriterApplication.java
@@ -0,0 +1,13 @@
+package toyProject.sixWordsWriter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SixWordsWriterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SixWordsWriterApplication.class, args);
+	}
+
+}
---
+++ sixWordsWriter/src/test/java/toyProject/sixWordsWriter/SixWordsWriterApplicationTests.java
@@ -0,0 +1,13 @@
+package toyProject.sixWordsWriter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SixWordsWriterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
